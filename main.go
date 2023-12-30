a111b4cff31b47f3f5ab666f109df73c4d5256d8
Taylor Glaeser <tzglaeser@gmail.com>
2023-12-30 13:53:09 -0600

    Added the ability to customize some mDNS fields.

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"context"
+	"flag"
+	"github.com/gokrazy/gokrazy"
 	"log"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/brutella/dnssd"
 	dnssdlog "github.com/brutella/dnssd/log"
-	"github.com/gokrazy/gokrazy"
 )
 
 func main() {
@@ -16,6 +18,32 @@ func main() {
 
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
+
+	/* TODO: Add flags for other customizable fields
+	dnssd.Service{
+		Name:   "",
+		Type:   "",
+		Domain: "",
+		Host:   "",
+		Text:   nil,
+		TTL:    0,
+		Port:   0,
+		IPs:    nil,
+		Ifaces: nil,
+	}
+	*/
+
+	var Host string
+	var Domain string
+	var Ifaces []string
+
+	var IfacesStr string
+
+	flag.StringVar(&Host, "host", "gokrazy", "mDNS host")
+	flag.StringVar(&Domain, "domain", "local", "mDNS domain")
+	flag.StringVar(&IfacesStr, "ifaces", "eth0,wlan0", "broadcast interfaces (comma-separated)")
+
+	Ifaces = strings.Split(IfacesStr, ",")
 
 	// Wait until network interfaces have a chance to work.
 	gokrazy.WaitForClock()
@@ -26,9 +54,9 @@ func main() {
 	}
 	// Just need the A record.
 	_, err = resp.Add(dnssd.Service{
-		Domain: "local",
-		Host:   "gokrazy",
-		Ifaces: []string{"eth0", "wlan0"},
+		Domain: Domain,
+		Host:   Host,
+		Ifaces: Ifaces,
 	})
 	if err != nil {
 		log.Fatalf("add service: %s", err)
